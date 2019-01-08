7662a6576599fcc0fcde33331ebf3f2e78a4fcfd
Björn Persson <41211330+Qalalf@users.noreply.github.com>
2019-01-08 10:08:09 +0100

    changed structure in homefolder

---
+++ src/main/java/com/nerdblistersteam/concierge/ConciergeApplication.java
@@ -0,0 +1,14 @@
+package com.nerdblistersteam.concierge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConciergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConciergeApplication.class, args);
+	}
+
+}
+
