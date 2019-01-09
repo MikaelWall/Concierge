a6c6de1b33a72871c4dae42cf138e5394fe8cdf5
henrikjohansson712 <henrik.johansson712@gmail.com>
2019-01-09 12:03:13 +0100

    Lade till några buggar

---
+++ src/main/java/com/nerdblistersteam/concierge/config/AuditorAwareImpl.java
@@ -0,0 +1,13 @@
+package com.nerdblistersteam.concierge.config;
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/com/nerdblistersteam/concierge/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.nerdblistersteam.concierge.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsServiceImpl userDetailsService;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+}
