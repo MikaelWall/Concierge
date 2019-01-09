7d08e30ef25a8e0de3906f4ddec5979779fbf054
henrikjohansson712 <henrik.johansson712@gmail.com>
2019-01-09 09:44:46 +0100

    Ändrade saker

--- src/main/java/com/nerdblistersteam/concierge/controller/AuthController.java
+++ src/main/java/com/nerdblistersteam/concierge/controller/AuthController.java
@@ -1,7 +1,13 @@
 package com.nerdblistersteam.concierge.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
+
+    @GetMapping("login")
+    public String login() {
+        return "auth/login";
+    }
 }
