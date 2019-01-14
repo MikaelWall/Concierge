1b944e4109c1e034aaf5239c4718f5656bfe12d5
Björn Persson <41211330+Qalalf@users.noreply.github.com>
2019-01-14 11:21:44 +0100

    Lagt in delete och fixat koppling till css-filerna

--- src/main/java/com/nerdblistersteam/concierge/controller/ConciergeController.java
+++ src/main/java/com/nerdblistersteam/concierge/controller/ConciergeController.java
@@ -1,13 +1,9 @@
 package com.nerdblistersteam.concierge.controller;
 
-import com.nerdblistersteam.concierge.domain.Description;
 import com.nerdblistersteam.concierge.domain.Room;
-import com.nerdblistersteam.concierge.repository.DescriptionRepository;
-import com.nerdblistersteam.concierge.repository.RoomRepository;
 import com.nerdblistersteam.concierge.service.RoomService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,13 +15,12 @@ import java.util.Optional;
 @Controller
 public class ConciergeController {
 
-
     private final Logger logger = LoggerFactory.getLogger(ConciergeController.class);
-    @Autowired
-    private RoomRepository roomRepository;
+    private RoomService roomService;
 
-    @Autowired
-    private DescriptionRepository descriptionRepository;
+    public ConciergeController(RoomService roomService) {
+        this.roomService = roomService;
+    }
 
     @GetMapping("/")
     public String index() {
@@ -33,17 +28,37 @@ public class ConciergeController {
     }
 
     @GetMapping("/createroom")
-    public String createroom() {
+    public String createroom(Model model) {
+        model.addAttribute("rooms", roomService.findAll());
         return "createroom";
     }
 
+
+
     @PostMapping("/createroom")
-    public String createNewRoom(@RequestParam String name, @RequestParam int seats, @RequestParam String hdmi, @RequestParam String whiteboard) {
+    public String createNewRoom(@RequestParam String name, @RequestParam int seats) {
         Room newRoom = new Room(name, seats);
         //Description newDescription = new Description(seats);
-        roomRepository.save(newRoom);
+        roomService.save(newRoom);
        // descriptionRepository.save(newDescription);
         System.out.println("Skapat rum " + name);
         return "index";
     }
+
+    @PostMapping("/deleteroom")
+    public String deleteRoom(@RequestParam String deleteName, Model model) {
+        Optional<Room> findDelRoom = roomService.findByName(deleteName);
+        if (findDelRoom.isPresent()) {
+            Room delRoom = findDelRoom.get();
+            roomService.delete(delRoom);
+            model.addAttribute("success", model.containsAttribute("success"));
+        } else {
+            model.addAttribute("danger", model.containsAttribute("danger"));
+        }
+
+        return "redirect:/createroom";
+    }
+
+
+
 }
--- src/main/java/com/nerdblistersteam/concierge/service/RoomService.java
+++ src/main/java/com/nerdblistersteam/concierge/service/RoomService.java
@@ -2,9 +2,12 @@ package com.nerdblistersteam.concierge.service;
 
 import com.nerdblistersteam.concierge.domain.Room;
 import com.nerdblistersteam.concierge.repository.RoomRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
+@Service
 public class RoomService {
 
     private final RoomRepository roomRepository;
@@ -13,11 +16,19 @@ public class RoomService {
         this.roomRepository = roomRepository;
     }
 
+    public List<Room> findAll(){
+        return roomRepository.findAll();
+    }
+
     public Optional<Room> findByName(String name) {
         return roomRepository.findByName(name);
     }
 
     public Room save(Room room) {
         return roomRepository.save(room);
+    }
+
+    public void delete(Room room) {
+        roomRepository.delete(room);
     }
 }
