b3d530276377f9e50eb9e2f398726dcaf3121d7c
kherota <kherota1@icloud.com>
2019-01-15 11:13:03 +0100

    fixat lite

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.header').height($(window).height());
-   })
+})
 
-   $(".navbar a").click(function(){
+$(".navbar a").click(function () {
     $("body,html").animate({
-     scrollTop:$("#" + $(this).data('value')).offset().top
-    },1000)
-    
-   })
+        scrollTop: $("#" + $(this).data('value')).offset().top
+    }, 1000)
+
+})
+$(function () {
+    $('#profile-image1').on('click', function () {
+        $('#profile-image-upload').click();
+    });
+});
+
+$(document).ready(function(){
+    $("#edit-info").on("click",function(){
+        
+        $(".info-section").removeClass("no-edit-forms");
+       $(this).hide();
+       $("#cancel-info").removeClass("hide");
+    });
+    $("#cancel-info").on("click",function(){
+        window.location.reload();
+        
+    });
+     $(".upload-image").dropzone();
+ });
