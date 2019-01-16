7920294c5c599c0e46d3b3670f02110b48a8bbdd
henrikjohansson712 <henrik.johansson712@gmail.com>
2019-01-16 20:34:35 +0100

    Flyttade invitemappningarna till en egen controller

--- src/main/java/com/nerdblistersteam/concierge/controller/ConciergeController.java
+++ src/main/java/com/nerdblistersteam/concierge/controller/ConciergeController.java
@@ -1,22 +1,15 @@
 package com.nerdblistersteam.concierge.controller;
 
-import com.nerdblistersteam.concierge.domain.Invited;
 import com.nerdblistersteam.concierge.domain.Room;
-import com.nerdblistersteam.concierge.domain.User;
 import com.nerdblistersteam.concierge.service.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -25,17 +18,11 @@ public class ConciergeController {
     private final Logger logger = LoggerFactory.getLogger(ConciergeController.class);
     private RoomService roomService;
     private ScheduleService scheduleService;
-    private UserService userService;
-    private InvitedService invitedService;
-    private RoleService roleService;
 
 
-    public ConciergeController(RoomService roomService, ScheduleService scheduleService, UserService userService, InvitedService invitedService, RoleService roleService) {
+    public ConciergeController(RoomService roomService, ScheduleService scheduleService) {
         this.roomService = roomService;
         this.scheduleService = scheduleService;
-        this.userService = userService;
-        this.invitedService = invitedService;
-        this.roleService = roleService;
     }
 
     @GetMapping("/")
@@ -83,48 +70,4 @@ public class ConciergeController {
 
         return "redirect:/createroom";
     }
-
-//    @Secured({"ROLE_ADMIN"})
-    @GetMapping("/invite")
-    public String invite(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        model.addAttribute("addedByFullName", ((User)authentication.getPrincipal()).getFullName());
-        Optional<User> currentUserFind = userService.findByEmail("admin@gmail.com");
-        if (currentUserFind.isPresent()) {
-            model.addAttribute("user", currentUserFind.get());
-        }
-        model.addAttribute("invited", invitedService.findAll());
-//        System.out.println(((User)authentication.getPrincipal()).getFullName());
-        return "invite";
-    }
-
-    @PostMapping("/addInvite")
-    public String addInvite(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String addedByFullName, @RequestParam boolean isAdmin, RedirectAttributes redirectAttributes) {
-        invitedService.invite(email, firstName, lastName, addedByFullName, isAdmin);
-        return "redirect:/invite";
-    }
-
-    @PostMapping("/deleteInvite")
-    public String deleteInvite(@RequestParam String email, Model model) {
-        Optional<Invited> invitedDeleteFind = invitedService.findByEmail(email);
-        if (invitedDeleteFind.isPresent()) {
-            Invited invitedDelete = invitedDeleteFind.get();
-            invitedService.delete(invitedDelete);
-            model.addAttribute("success", model.containsAttribute("success"));
-        } else {
-            model.addAttribute("danger", model.containsAttribute("danger"));
-        }
-        return "redirect:/invite";
-    }
-
-    @PostMapping("/sendInvite")
-    public String sendInvite(RedirectAttributes redirectAttributes, Model model) {
-        List<Invited> invitedList = invitedService.findAll();
-        for (Invited invited : invitedList) {
-            invitedService.sendInvitationEmail(invited);
-        }
-        redirectAttributes.addFlashAttribute("success", true);
-        return "redirect:/invite";
-    }
-
 }
---
+++ src/main/java/com/nerdblistersteam/concierge/controller/InviteController.java
@@ -0,0 +1,69 @@
+package com.nerdblistersteam.concierge.controller;
+
+import com.nerdblistersteam.concierge.domain.Invited;
+import com.nerdblistersteam.concierge.domain.User;
+import com.nerdblistersteam.concierge.service.InvitedService;
+import com.nerdblistersteam.concierge.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class InviteController {
+
+    private final UserService userService;
+    private final InvitedService invitedService;
+
+    public InviteController(UserService userService, InvitedService invitedService) {
+        this.userService = userService;
+        this.invitedService = invitedService;
+    }
+
+    @GetMapping("/invite")
+    public String invite(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        model.addAttribute("addedByFullName", ((User)authentication.getPrincipal()).getFullName());
+        Optional<User> currentUserFind = userService.findByEmail("admin@gmail.com");
+        currentUserFind.ifPresent(user -> model.addAttribute("user", user));
+        model.addAttribute("invited", invitedService.findAll());
+//        System.out.println(((User)authentication.getPrincipal()).getFullName());
+        return "invite";
+    }
+
+    @PostMapping("/addInvite")
+    public String addInvite(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String addedByFullName, @RequestParam boolean isAdmin, RedirectAttributes redirectAttributes) {
+        invitedService.invite(email, firstName, lastName, addedByFullName, isAdmin);
+        return "redirect:/invite";
+    }
+
+    @PostMapping("/deleteInvite")
+    public String deleteInvite(@RequestParam String email, RedirectAttributes redirectAttributes) {
+        Optional<Invited> invitedDeleteFind = invitedService.findByEmail(email);
+        if (invitedDeleteFind.isPresent()) {
+            Invited invitedDelete = invitedDeleteFind.get();
+            invitedService.delete(invitedDelete);
+            redirectAttributes.addFlashAttribute("deleted", true);
+        } else {
+            redirectAttributes.addFlashAttribute("deleted", false);
+        }
+        return "redirect:/invite";
+    }
+
+    @PostMapping("/sendInvite")
+    public String sendInvite(RedirectAttributes redirectAttributes, Model model) {
+        List<Invited> invitedList = invitedService.findAll();
+        for (Invited invited : invitedList) {
+            invitedService.sendInvitationEmail(invited);
+        }
+        redirectAttributes.addFlashAttribute("success", true);
+        return "redirect:/invite";
+    }
+}
